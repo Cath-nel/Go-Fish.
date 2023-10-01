1e41cb96d91099525ba039ca26e79bea93313fdc
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-01 17:05:51 +0200

    Create Suite.java

---
+++ Suite.java
@@ -0,0 +1,6 @@
+public class Suite{
+  public static final int SUITE_HEARTS = 1;
+  public static final int SUITE_DIAMONDS = 2;
+  public static final int SUITE_CLUBS = 3;
+  public static final int SUITE_SPADES = 4;
+}
