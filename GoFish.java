83cbc32d0596c58bf9a649e413140d56569e6329
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-01 17:25:18 +0200

    Create GoFish.java

---
+++ GoFish.java
@@ -0,0 +1,11 @@
+public class GoFish{
+  public static int DECKSIZE;
+	private boolean isGameDone;
+	public static int DECK;
+	private int askedCard;
+	private int books;
+	private int cardsAmount;
+
+	public GoFish(){
+	}
+}
