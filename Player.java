059fff1847bfb66d75985fca03bd2e3ace1f8c99
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-01 17:21:59 +0200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player{
+	private String playerID; 
+	private String playerName; 
+	private Hand hand;
+
+	public Player(String playerID, String playerName, Hand hand){
+		this.playerID = playerID;
+		this.player = playerName;
+		this.hand = hand;
+	}
+}
