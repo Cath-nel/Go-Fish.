f4ea441a7a141a2f13d500732476a9f1161e45cc
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-05 23:05:21 +0200

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,6 +1,7 @@
 public class Hand{
   private int playerHand; //what is this for
   private List<Card> cards;
+	private int numCards;
 	private String playerID;
 	private Player player;
 	private String playerName; //I dont think we need this cause we can pull it from the player object
@@ -8,7 +9,39 @@ public class Hand{
 
 	public Hand(Player player){
 		this.player = player;
+		this.playerID = player.getPlayerID;
+		this.playerName = player.getPlayerName;
 		//give random or sequentially next int
+	}
+
+	public void setHand(Card cards){
+		this.cards = cards;
+		numCards = card.size();
+	}
+
+	public boolean checkForCard(int requestedRank){
+		for(int i =0; i<numCards; i++){
+			if(cards.get(i).getRank()==requestedRank){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Card getCard(int requestedRank){
+	//returns the requested card object, first one that matches rank
+	//remove said card for the list of cards
+		if(checkForCard(requestedRank)){
+			for(int i =0; i<numCards; i++){
+				if(cards.get(i).getRank()==requestedRank){
+					Card result = cards.get(i);
+					cards.remove(i);
+					numCards--;
+					return result;
+				}
+			}
+		}
+		return null;
 	}
 
 	public boolean isEmpty(){
