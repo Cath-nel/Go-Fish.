429293855fdf74864fc8af587a2e4b626bb2655a
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-07 00:08:01 +0200

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -93,7 +93,7 @@ public class Hand{
 		int[] frequency = new int[13];
 		for(int i = 0; i<13; i++){
 			for(int j =0; j<numCards; i++){
-				if(cards.get(j).getRank()==i){
+				if(cards.get(j).getRank()==i+1){
 					frequency[i]++;
 				}
 			}
