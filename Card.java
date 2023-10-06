462f8c39d64bb2f336cb09fcd6ec60552856d76b
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-07 00:19:40 +0200

    Update Card.java

--- Card.java
+++ Card.java
@@ -23,6 +23,54 @@ public class Card{
 		//presumably for user interface?? although may have different purpose as java code will not reflect user interface
 	}
 
+	public static String formalRank(int numRank){
+		String result = "";
+
+		switch(numRank){
+			case 1:
+				result = "Ace";
+				break;
+			case 2:
+				result = "Two";
+				break;
+			case 3:
+				result = "Three";
+				break;
+			case 4:
+				result = "Four";
+				break;
+			case 5:
+				result = "Five";
+				break;
+			case 6:
+				result = "Six";
+				break;
+			case 7:
+				result = "Seven";
+				break;
+			case 8:
+				result = "Eight";
+				break;
+			case 9:
+				result = "Nine";
+				break;
+			case 10:
+				result = "Ten";
+				break;
+			case 11:
+				result = "Jack";
+				break;
+			case 12:
+				result = "Queen";
+				break;
+			case 13:
+				result = "King";
+				break;
+
+		}
+		return result;
+	}
+
 	public String toString(){
 		String result = "";
 
