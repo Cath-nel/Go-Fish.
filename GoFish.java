b0049ec55888db69916994436c557e8f1d0e3f9d
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-06 18:42:11 +0200

    Update GoFish.java

--- GoFish.java
+++ GoFish.java
@@ -5,7 +5,7 @@ public class GoFish{
 	private List<Card> cards = {"Ace", "2","3","4","5","6","7","8","9","10"","Jack","Queen", "King","Ace", "2","3","4","5","6","7","8","9","10"","Jack","Queen", "King","Ace", "2","3","4","5","6","7","8","9","10"","Jack","Queen", "King","Ace", "2","3","4","5","6","7","8","9","10"","Jack","Queen", "King"};
 	public static int GOFISH_DECKSIZE = 7;
 	private Hand mainDeck; //using a Hand object to store the remaining cards in the deck
-
+	
 	//Player1
 	private Player user; //needs to be instantiated in Program class
 	private int userBooks;
@@ -45,7 +45,25 @@ public class GoFish{
 	}
 	
 	public void allocateCards(){
-		
+		List<Cards> playerCards = new ArrayList<>;
+		List<Cards> compCards = new ArrayList<>;
+		for(int i = 0; i < 10; i++){
+			if(i%2==0){
+				Card current = cards.get(i);
+				cards.remove(i);
+				//numCards--;
+				playerCards.add(current);
+			}else{
+				Card current = cards.get(i);
+				cards.remove(i);
+				//numCards--;
+				compCards.add(current);
+			}
+		}
+		Hand userHand = new Hand(user);
+		userHand.getCards(playerCards);
+		Hand compHand = new Hand(computer);
+		compHand.getCards(compCards);
 	}
 
 	public void askCard(Card askedCard){ //I've used a card object here as presumably we'd be able to get that from GUI?
