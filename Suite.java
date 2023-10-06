28d3892aeac5418846e56c509d493705c90a37f3
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-06 18:29:41 +0200

    Update Suite.java

--- Suite.java
+++ Suite.java
@@ -3,4 +3,23 @@ public class Suite{
   public static final int SUITE_DIAMONDS = 2;
   public static final int SUITE_CLUBS = 3;
   public static final int SUITE_SPADES = 4;
+
+  public static String getSuite(int num){
+      switch(num){
+        case Suite.SUITE_HEARTS:
+              return "Hearts";
+          break;
+          case Suite.SUITE_DIAMONDS:
+              return "Diamonds";
+          break;
+          case Suite.SUITE_CLUBS:
+              return "Clubs";
+          break;
+          case Suite.SUITE_SPADES:
+              return "Spades";
+          break;
+        default:
+          return "Invalid input";
+      }
+  }
 }
