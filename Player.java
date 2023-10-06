05ab12e6726e53af4af34c98f08d53edf03b3777
22tarynb <143820704+22tarynb@users.noreply.github.com>
2023-10-06 17:55:15 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,16 +2,24 @@ public class Player{
 	private String playerID; 
 	private String playerName; 
 	private Hand hand;
+	private int numBooks;
+	private List<List<Card>> books;
+	
 
 	public Player(String playerID, String playerName, Hand hand){
 		this.playerID = playerID;
 		this.player = playerName;
 		this.hand = hand;
 	}
-	public String getPlayerIDe(){
+	public String getPlayerID(){
 		return playerID;
 	}
 	public String getPlayerName(){
 		return playerName;
+	}
+
+	public void addBook(List<Card> book){
+		books.add(book);
+		numBooks++;
 	}
 }
